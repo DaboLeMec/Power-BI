02afcdab68506f039b50d2b4bb1d2fcd9c4cf537
DaboLeMec <Stephen.Gessling925@gmail.com>
2021-10-03 12:58:38 -0700

    Add files via upload
    Tabular Editor Advanced Script for developing a moving average measure

---
+++ Moving Avg Scripts.cs
@@ -0,0 +1,9 @@
+// Creates a SUM measure for every currently selected column and hide the column.
+foreach(var m in Selected.Measures)
+{
+    var newMeasure = m.Table.AddMeasure(
+    "3 Week Moving Avg" + m.Name,                    // Name
+    "VAR Week = 4 RETURN DIVIDE( SUMX( FILTER(  ALL( '_Date' ), _Date[RelativeFiscalWeekNumber] > MAX( _Date[RelativeFiscalWeekNumber] ) - Week &&_Date[RelativeFiscalWeekNumber] <= MAX( _Date[RelativeFiscalWeekNumber] )  )," + m.DaxObjectFullName + ") , Week )",
+     m.DisplayFolder =   "_Moving Avgs"                   // Display Folder
+    );
+}
