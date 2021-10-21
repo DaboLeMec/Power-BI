793dac27eca0a1c90e62561dc68954836640b7ad
Steve Gessling <Stephen.Gessling925@gmail.com>
2021-10-21 09:26:48 -0700

    Update Moving Avg Scripts.cs

--- Moving Avg Scripts.cs
+++ Moving Avg Scripts.cs
@@ -3,7 +3,7 @@ foreach(var m in Selected.Measures)
 {
     var newMeasure = m.Table.AddMeasure(
     "3 Week Moving Avg" + m.Name,                    // Name
-    "VAR Week = 4 RETURN DIVIDE( SUMX( FILTER(  ALL( '_Date' ), _Date[RelativeFiscalWeekNumber] > MAX( _Date[RelativeFiscalWeekNumber] ) - Week &&_Date[RelativeFiscalWeekNumber] <= MAX( _Date[RelativeFiscalWeekNumber] )  )," + m.DaxObjectFullName + ") , Week )",
+    "VAR Week = 3 RETURN DIVIDE( SUMX( FILTER(  ALL( '_Date' ), _Date[RelativeFiscalWeekNumber] > MAX( _Date[RelativeFiscalWeekNumber] ) - Week &&_Date[RelativeFiscalWeekNumber] <= MAX( _Date[RelativeFiscalWeekNumber] )  )," + m.DaxObjectFullName + ") , Week )",
      m.DisplayFolder =   "_Moving Avgs"                   // Display Folder
     );
     // Set the format string on the new measure:
